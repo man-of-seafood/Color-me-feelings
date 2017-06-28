e3435cfae9a12d2fcb0493652412b213bd1ae347
r-i-c-h <rich@richwerden.com>
2017-06-28 14:21:16 -0700

    Moves Watson calls into their own file (and adds a preemptive db filler file)

--- server/index.js
+++ server/index.js
@@ -4,31 +4,13 @@ var bodyParser = require('body-parser');
 var axios = require('axios');
 var configFile = require('../config/config'); // PRIVATE FILE - DO NOT COMMIT!
     var secret = configFile.keys;
-var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
-var items = require('../database-mongo');
+var db = require('../database-mongo');
+var refill = require('./addArticles');
+var analyze = require('./callWatson');
+
 
 app.use(express.static(__dirname + '/../react-client/dist'));
-
-// create instance of Tone Analyzer service
-var toneAnalyzer = new ToneAnalyzerV3({
-  username: secret.WATSON_API_USERNAME,
-  password: secret.WATSON_API_PASS,
-  version_date: '2016-05-19'
-});
-
-var params = {
-  // Get the text from the JSON file.
-  text: require(__dirname + '/../apis/news-sample.json').text,
-  tones: 'emotion', // omit for all three, comma separated list
-  sentences: false
-};
-
-// UNCOMMENT FOR Watson call
-// toneAnalyzer.tone(params, function(error, response) {
-//   error ? console.log('error:', error) : console.log(JSON.stringify(response, null, 2));
-// });
-
 
 app.get('/items', function (req, res) {
   items.selectAll(function(err, data) {
