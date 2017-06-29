b296ace68cf86297ecf8f8f5bca69df953615b16
Bryant Huang <b.luv.sic@gmail.com>
2017-06-29 16:29:51 -0700

    add cron job , and moved legend around

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ var secret = configFile.keys;
 var db = require('../database-mongo');
 var refill = require('./addArticles');
 var analyze = require('./callWatson');
+var CronJob = require('cron').CronJob;
 
 
 app.use(express.static(__dirname + '/../react-client/dist'));
@@ -21,8 +22,21 @@ app.get('/items', function (req, res) {
     }
   });
 });
-
-
+//just require anywhere you want to start a job and change crontime based on what you want
+var job = new CronJob({
+  cronTime: '00 30 11 * * 1,5',
+  onTick: function() {
+    /* run whatever you want scheduled in here.
+     * Runs every weekday (Monday and Friday)
+     * at 11:30:00 AM. 
+     */
+  }, function() {
+    //can remove, but runs when job is finihsed
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+job.start();
 /*~~~~~~~~~~~~~~~~~~~~~~~ STARTUP SERVER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 app.listen(3000, function() {
   console.log('listening on port 3000!');
