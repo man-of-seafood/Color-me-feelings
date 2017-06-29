0112e41dc514194127f983cdeb0d9c026e807642
Andrea <andreamiralles686@gmail.com>
2017-06-29 12:37:45 -0700

    require dic

--- server/callWatson.js
+++ server/callWatson.js
@@ -2,6 +2,7 @@ var db = require('../database-mongo');
 var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 var configFile = require('../config/config'); // PRIVATE FILE - DO NOT COMMIT!
 var secret = configFile.keys;
+var dictionary = require('../database-mongo/dictionary'); // stateCodeArr, stateNameArr, dictionary
 
 // create instance of Tone Analyzer service
 var toneAnalyzer = new ToneAnalyzerV3({
@@ -20,8 +21,10 @@ var params = {
 // Get each article from db >>> Call to Watson >>> Update each article with 5-scores based on the response...
 
 // UNCOMMENT FOR Watson call
-// toneAnalyzer.tone(params, function(error, response) {
-//   error ? console.log('error:', error) : console.log(JSON.stringify(response, null, 2));
-// });
+
+
+toneAnalyzer.tone(params, function(error, response) {
+  error ? console.log('error:', error) : console.log(JSON.stringify(response, null, 2));
+});
 
 module.exports; // ?????  // I called this file 'analyze' in the index.js ¯\_(ツ)_/¯ --RW
