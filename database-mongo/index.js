06c76d0eeb99a580cf7986e91ea38b7a8ec8c7dd
r-i-c-h <rich@richwerden.com>
2017-06-29 10:41:07 -0700

    puts in basic server Article model

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,31 +1,42 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.promise;
 
+const dbURI = 'mongodb://localhost/NewsDB';
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+mongoose.connect(dbURI);
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'db connection error!'));
+db.once('open', function(){ console.log('mongoose connected successfully'); });
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-db.on('error', function() {
-  console.log('mongoose connection error');
-});
+var articleSchema = new Schema({
+  uuid: { type: String, required: true},
+  date: { type: Date, default: Date.now },
+  stateCode: { type: String, uppercase: true, minlength: 2, maxlength: 2 },
+  text: String,
+  processed: { type: Boolean, default: false },
+  scores: {
+    joy: { type: Number, default: null },
+    anger: { type: Number, default: null },
+    disgust: { type: Number, default: null },
+    fear: { type: Number, default: null },
+    sadness: { type: Number, default: null }
+  }
+},{minimize: false});
 
-db.once('open', function() {
-  console.log('mongoose connected successfully');
-});
 
-var itemSchema = mongoose.Schema({
-  quantity: Number,
-  description: String
-});
+var Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
 
-var Item = mongoose.model('Item', itemSchema);
+// var selectAll = function(callback) {
+//   Item.find({}, function(err, items) {
+//     if(err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
-};
-
-module.exports.selectAll = selectAll;
+// module.exports.selectAll = selectAll;
