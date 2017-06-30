7e0993363af8115e921cd8f1e34cc2f557a82fa4
r-i-c-h <rich@richwerden.com>
2017-06-30 13:47:23 -0700

    Adds pseudocode clarification of desired process

--- server/addArticles.js
+++ server/addArticles.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var Article = require('../database-mongo/index');
+var dbSetup = require('../database-mongo/index');
+    var Article = dbSetup.Article;
 var axios = require('axios');
 var bodyParser = require('body-parser'); // ??
 var reference = require('../database-mongo/dictionary');
@@ -11,7 +12,7 @@ var searchString = function(stateCode){
   var fullTextName = reference.dictionary[stateCode];
       fullTextName.replace(/\s/g, '%20');
 
-/* for TIME: Must be in Unix ms which are 13-integers long! */
+  /* for TIME: Must be in Unix ms which are 13-integers long! */
   var timeNow = new Date().getTime(); // time in Unix Epoch ms...
   var twoDaysAgo = timeNow - 86400000 - 86400000; // 86400000ms in a day
   return 'http://webhose.io/filterWebContent?token=e6d255ff-1622-4b08-9098-81186a9ca40e&format=json&ts=' +
@@ -26,15 +27,29 @@ var searchString = function(stateCode){
 var getStateData = function(stateCode){
     var queryString = searchString(stateCode);
     axios.get(queryString)
-    .then( (result) => { console.log('result from hose:'); } )
+    //?? mongoimport --db db_name --collection coll-name --type json --file seed.json --jsonArray ??
+
+    .then( (result) => { console.log('result from hose:',result);
+      // parse each article from response
+
+      // check the number of responses!
+
+      // for each article make fill the schema pattern and save -
+      //  var esto = new Article({uuid: , date:, stateCode:stateCode, text:}
+      //  esto.save(function (err) {
+      //    if (err) { console.error(err); }
+      //    // otherwise...
+      //    console.log(uuid + ' saved');
+      //  })
+    })
     .catch( (error) => { console.error(" UH OH!!! -->", error); } );
 };
 
 var dailyRefresh = function(){
-    var dbServer = 'mongodb://localhost/NewsFile';
-    var db = mongoose.createConnection(dbServer);
+    // var dbServer = 'mongodb://localhost/NewsFile';
+    // var db = mongoose.createConnection(dbServer);
 
-    db.dropDatabase();
+    Article.find().remove();
 
     statesList.forEach((stateCode) => {
       getStateData(stateCode);
