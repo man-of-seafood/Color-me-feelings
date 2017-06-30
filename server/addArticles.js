9986783cd0c25e49b8a41fb006e17327664b5a16
r-i-c-h <rich@richwerden.com>
2017-06-30 12:47:22 -0700

    revises query string and cleans up code

--- server/addArticles.js
+++ server/addArticles.js
@@ -7,27 +7,20 @@ var reference = require('../database-mongo/dictionary');
 var configFile = require('../config/config'); // PRIVATE FILE - DO NOT COMMIT!
     var WEBHOSE_API_KEY = configFile['WEBHOSE_API_KEY'];
 
-// var dbServer = 'mongodb://localhost/NewsFile';
-// var db = mongoose.createConnection(dbServer);
-// db.dropDatabase();
-
 var searchString = function(stateCode){
   var fullTextName = reference.dictionary[stateCode];
       fullTextName.replace(/\s/g, '%20');
-/*
-For Thu, 29 Jun 2017 00:00:00 PST -->> Epoch timestamp: 1498719600
-Human time (PST): Thursday, June 29, 2017 12:00:00 AM GMT-07:00
-Human time (GMT): Thursday, June 29, 2017 7:00:00 AM
-            Timestamp in milliseconds: 1498719600000
 
-For Wed, 28 Jun 2017 00:00:00 PST -->> Epoch timestamp: 1498633200
-Human time (PST): Wednesday, June 28, 2017 12:00:00 AM GMT-07:00
-Human time (GMT): Wednesday, June 28, 2017 7:00:00 AM
-            Timestamp in milliseconds: 1498633200000
-*/
-  var timeNow = new Date().getTime(); // time in Unix secs...
-  var twoDaysAgo = timeNow - 86400000 - 86400000; // 86400secs in a day or 86400000ms
-  return 'http://webhose.io/filterWebContent?token=e6d255ff-1622-4b08-9098-81186a9ca40e&format=json&ts='+ timeNow + '&sort=published&q=language%3Aenglish%20published%3A%3E'+ twoDaysAgo +'%20site_type%3Anews%20performance_score%3A%3E5%20location%3A%22' + fullTextName + '%22';
+/* for TIME: Must be in Unix ms which are 13-integers long! */
+  var timeNow = new Date().getTime(); // time in Unix Epoch ms...
+  var twoDaysAgo = timeNow - 86400000 - 86400000; // 86400000ms in a day
+  return 'http://webhose.io/filterWebContent?token=e6d255ff-1622-4b08-9098-81186a9ca40e&format=json&ts=' +
+         twoDaysAgo +
+         '&sort=published&q=is_first%3Atrue%20language%3Aenglish%20published%3A%3E' +
+         twoDaysAgo +
+         '%20site_type%3Anews%20thread.country%3AUS%20performance_score%3A%3E7%20location%3A%22+' +
+         fullTextName +
+         '%22';
 };
 
 var getStateData = function(stateCode){
@@ -37,7 +30,15 @@ var getStateData = function(stateCode){
     .catch( (error) => { console.error(" UH OH!!! -->", error); } );
 };
 
-module.exports = getStateData;
+var dailyRefresh = function(){
+    var dbServer = 'mongodb://localhost/NewsFile';
+    var db = mongoose.createConnection(dbServer);
 
+    db.dropDatabase();
 
-// 13 places
+    statesList.forEach((stateCode) => {
+      getStateData(stateCode);
+    });
+};
+
+module.exports = dailyRefresh;
