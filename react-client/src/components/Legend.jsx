e23c9e0163b73bd4a1f16e06787368684a0028f0
Bryant Huang <b.luv.sic@gmail.com>
2017-07-03 15:40:17 -0700

    update legend names

--- react-client/src/components/Legend.jsx
+++ react-client/src/components/Legend.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Legend = (props) => (
   <div id='state-legend' className='legend'>
-    <h4>Legend</h4>
+    <h3>Level of {props.emotion[0].toUpperCase() + props.emotion.slice(1)}</h3>
     <div><span style={{backgroundColor: props.color[0]}}></span>0</div>
     <div><span style={{backgroundColor: props.color[1]}}></span>25</div>
     <div><span style={{backgroundColor: props.color[2]}}></span>50</div>
