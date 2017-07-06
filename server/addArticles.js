15fee6b6e9882829773dc053afb580b4a57b7c8b
Brian Wong <brianwong.se@gmail.com>
2017-07-05 20:45:01 -0700

    Use webhose module. Start topic refactor

--- server/addArticles.js
+++ server/addArticles.js
@@ -1,19 +1,20 @@
 const mongoose = require('mongoose');
 const axios = require('axios');
+const WEBHOSE_API_KEY = require('../config/config').WEBHOSE_API_KEY; // PRIVATE FILE - DO NOT COMMIT!
+const webhose = require('webhoseio').config({ token: WEBHOSE_API_KEY });
 const Article = require('../database/models/Article');
 const dictionary = require('../database/dictionary').dictionary;
-const config = require('../config/config'); // PRIVATE FILE - DO NOT COMMIT!
 const stateCodes = require('../database/dictionary').stateCodeArr;
-const WEBHOSE_API_KEY = config['WEBHOSE_API_KEY'];
 
-const getSearchStr = stateCode => {
-  const fullStateName = dictionary[stateCode].replace(/\s/g, '%20');
+const getSearchStr = (topic, stateCode) => {
+  const stateName = dictionary[stateCode].replace(/\s/g, '%20');
   const twoDaysAgo = Date.now() - 2 * 86400000; // 86400000ms in a day
-  return (
-    `http://webhose.io/filterWebContent?token=${WEBHOSE_API_KEY}&format=json&ts=${twoDaysAgo}` +
-    `&sort=published&q=is_first%3Atrue%20language%3Aenglish%20published%3A%3E${twoDaysAgo}` +
-    `%20site_type%3Anews%20thread.country%3AUS%20performance_score%3A%3E7%20location%3A%22${fullStateName}%22`
-  );
+  return `'${topic}' language:english thread.country:US location:${stateName}`;
+  // return (
+  //   `http://webhose.io/filterWebContent?token=${WEBHOSE_API_KEY}&format=json&ts=${twoDaysAgo}` +
+  //   `&sort=published&q=is_first%3Atrue%20language%3Aenglish%20published%3A%3E${twoDaysAgo}` +
+  //   `%20site_type%3Anews%20thread.country%3AUS%20performance_score%3A%3E7%20location%3A%22${stateName}%22`
+  // );
 };
 
 const clearStateData = stateCode => {
@@ -23,32 +24,30 @@ const clearStateData = stateCode => {
 };
 
 const getStateData = function(stateCode) {
-  const queryString = getSearchStr(stateCode);
+  const queryString = getSearchStr('war', stateCode); // todo: TOPIC
 
-  axios
-    .get(queryString)
+  webhose
+    .query(queryString)
     .then(result => {
-      const totalResults = result.data.totalResults;
-      console.log(totalResults + ' articles rec\'d for ' + stateCode + ' in hose-response');
+      const resultCount = result.data.totalResults;
+      console.log(resultCount + ' articles rec\'d for ' + stateCode + ' in hose-response');
 
-      if (totalResults > 0) {
-        clearStateData(stateCode);
-      } else {
-        console.log('ZERO NEW Articles for ' + stateCode + ', leaving STALE DATA as is.');
-      }
+      resultCount
+        ? clearStateData(stateCode)
+        : console.log('ZERO NEW Articles for ' + stateCode + ', leaving STALE DATA as is.');
 
-      const arrOfArticleObj = result.data.posts;
-      arrOfArticleObj.forEach(articleObj => {
+      const articles = result.data.posts;
+      articles.forEach(article => {
         const inbound = new Article({
-          uuid: articleObj.uuid,
-          date: articleObj.published,
+          uuid: article.uuid,
+          date: article.published,
           stateCode: stateCode,
-          text: articleObj.text,
-          title: articleObj.title,
-          url: articleObj.url
+          text: article.text,
+          title: article.title,
+          url: article.url
         });
         inbound.save(err => {
-          err ? console.error(err) : console.log('saved uuid-', articleObj.uuid);
+          err ? console.error(err) : console.log('saved uuid-', article.uuid);
         });
       });
     })
@@ -61,8 +60,8 @@ const dailyRefresh = () => {
   // TO REQUEST DATA FOR ALL STATES - COMMENT OUT LINES 67-68, AND UN-COMMENT LINE 70
   // const onlyFirstTenStates = stateCodes.slice(0,9);
   const onlyFirstState = stateCodes.slice(0, 1); //testing with one state
+  // stateCodes.forEach( (stateCode,i) => {
   onlyFirstState.forEach((stateCode, i) => {
-    // stateCodes.forEach( (stateCode,i) => {
     setTimeout(() => {
       getStateData(stateCode);
     }, i * 1000);
