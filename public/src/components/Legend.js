4b65f461da363c8a714c40c6a2fee28339ab284d
JD Broberg <jdbroberg@yahoo.com>
2017-07-06 18:37:02 -0700

    refactored legend to use css file

--- public/src/components/Legend.js
+++ public/src/components/Legend.js
@@ -1,40 +1,21 @@
 import React from 'react';
 
 const Legend = (props) => (
-  <div id='state-legend' style={style}>
-    <h3 style={titleStyle}>Level of {props.emotion[0].toUpperCase() + props.emotion.slice(1)}</h3>
-    <div><div style={colorStyle(props.color[0])}></div>0</div>
-    <div><div style={colorStyle(props.color[1])}></div>25</div>
-    <div><div style={colorStyle(props.color[2])}></div>50</div>
-    <div><div style={colorStyle(props.color[3])}></div>75</div>
-    <div><div style={colorStyle(props.color[4])}></div>100</div>
+  <div id='state-legend' className="legend">
+    <h3 className="legend-title">Level of {props.emotion[0].toUpperCase() + props.emotion.slice(1)}</h3>
+    {
+      props.color.map( (color, idx) => (
+        <div>
+          <div style={colorStyle(color)} key={idx} className="color-key"/>
+          {idx * 25}
+        </div>
+      ))
+    }
   </div>
 );
 
-const style = {
-  position: `absolute`,
-  right: `3em`,
-  bottom: `3em`,
-  backgroundColor: `#ffefd5`,
-  padding: `10px`,
-  borderRadius: `10px`,
-  fontFamily: `arial`,
-  width: `10em`,
-};
-
-const titleStyle = {
-  color: `#073642`
-}
-
 const colorStyle = (color) => ({
-  width: `1em`,
-  height: `1em`,
-  borderRadius: `0.2em`,
-  display: `inline-block`,
-  verticalAlign: `top`,
-  marginRight: `0.5em`,
-  marginBottom: `0.5em`,
-  backgroundColor: color,
+ backgroundColor: color
 });
 
 export default Legend;
